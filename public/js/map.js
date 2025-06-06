66f5c4da62417da95d51d627d3a8ca5e8d1b6744
Rohit <mprohit2005@gmail.com>
2025-06-07 00:11:27 +0530

    Done

--- public/js/map.js
+++ public/js/map.js
@@ -4,16 +4,16 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12',
-    center: listingToken.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 }); 
 
 
 const marker = new mapboxgl.Marker({ color: "red"})
-    .setLngLat(listingToken.geometry.coordinates)
+    .setLngLat(listing.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset:25 }).setHTML(
-            `<h4>${listingToken.title}</h4><p>Exact location will be provided after booking</p>`
+            `<h4>${listing.title}</h4><p>Exact location will be provided after booking</p>`
         )
     )
     .addTo(map);
